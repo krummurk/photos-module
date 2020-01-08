9cf7d68db98a816d54a179f240e218f965d6972b
mattviolet <mattviolet@gmail.com>
2020-01-08 13:39:14 -0800

    import styled components

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
 import Modal from './Modal.jsx';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 const PhotoContainer = styled.div`
   overflow: hidden;
